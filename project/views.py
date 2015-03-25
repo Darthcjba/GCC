295ebc6a09d5680dfbe0c7eeb3b6cbad51345776
Jordan <jayala1993@outlook.com>
2015-03-24 23:12:39 -0400

    Agregando un home de prueba (Sin bootstrap)

--- project/views.py
+++ project/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from project.models import MiembroEquipo, Proyecto
 
-# Create your views here.
+#Home simple para probar bootstrap
+def home(request):
+    context = {}
+    context['users'] = User.objects.all()
+    context['proyects'] = Proyecto.objects.all()
+    context['team_members'] = MiembroEquipo.objects.all()
+
+    return render(request, 'project/home.html', context)
