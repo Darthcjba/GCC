635af4dadd353dcc436a9cfd8ee4d13ee4c1ddfd
Jordan <jayala1993@outlook.com>
2015-03-25 15:09:45 -0400

    Agregando algunas vistas

--- project/urls.py
+++ project/urls.py
@@ -3,4 +3,8 @@ from project import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
+    url(r'^users/$', views.UserList.as_view(), name='user_list'),
+    url(r'^users/(?P<pk>\d+)/$', views.UserDetail.as_view(), name='user_detail'),
+    url(r'^projects/$', views.ProjectList.as_view(), name='project_list'),
+    url(r'^projects/(?P<pk>\d+)/$', views.ProjectDetail.as_view(), name='project_detail'),
 )
--- project/views.py
+++ project/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from project.models import MiembroEquipo, Proyecto
+from django.views.generic import ListView, DetailView
 
 #Home simple para probar bootstrap
 def home(request):
@@ -10,3 +11,24 @@ def home(request):
     context['team_members'] = MiembroEquipo.objects.all()
 
     return render(request, 'project/home.html', context)
+
+class UserList(ListView):
+    model = User
+    context_object_name = 'users'
+    template_name = 'project/user_list.html'
+
+class UserDetail(DetailView):
+    model = User
+    context_object_name = 'usuario'
+    template_name = 'project/user_detail.html'
+
+
+class ProjectList(ListView):
+    model = Proyecto
+    context_object_name = 'projects'
+    template_name = 'project/project_list.html'
+
+class ProjectDetail(DetailView):
+    model = Proyecto
+    context_object_name = 'project'
+    template_name = 'project/project_detail.html'
