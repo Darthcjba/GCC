e5d0674680903c02b9f70e2b059b7d8164959907
gperaltascura@gmail.com <gperaltascura@gmail.com>
2015-03-26 19:03:19 -0400

    más pruebas referentes a roles

--- project/tests.py
+++ project/tests.py
@@ -6,6 +6,15 @@ class RolesTest(TestCase):
         u = User.objects.create_user('temp','temp@email.com', 'temp')
         p = Permission.objects.get(codename='add_group')
         u.user_permissions.add(p)
+        p = Permission.objects.get(codename='change_group')
+        u.user_permissions.add(p)
+        p = Permission.objects.get(codename='delete_group')
+        u.user_permissions.add(p)
+
+    def create_role(self, name):
+        g = Group.objects.create(name=name)
+        g.save()
+        return g
 
     def test_create_role(self):
         c = self.client
@@ -29,3 +38,35 @@ class RolesTest(TestCase):
         #no deberia redirigir
         self.assertIsNot(response.status_code, 302)
         self.assertContains(response, 'This field is required.')
+
+    def test_edit_role(self):
+        c = self.client
+        self.assertTrue(c.login(username='temp', password='temp'))
+        g = self.create_role('developer')
+        #vemos que el rol no tiene los permisos de agregar proyecto
+        response = c.get('/roles/1/')
+        self.assertEquals(response.status_code, 200)
+        self.assertNotContains(response, 'Can add proyecto')
+
+        response = c.get('/roles/1/edit/')
+        self.assertEquals(response.status_code, 200)
+        #agregamos el permiso de crear proyecto
+        response = c.post('/roles/1/edit/', {'name':'developer','perms_proyecto':[u'add_proyecto']}, follow=True)
+        #deberia redirigir
+        self.assertRedirects(response, '/roles/1/')
+        #comprobamos que ahora aparece el permiso asignado
+        self.assertContains(response, 'Can add proyecto')
+
+    def test_delete_role(self):
+        c = self.client
+        self.assertTrue(c.login(username='temp', password='temp'))
+        g = self.create_role('developer')
+        #vemos que el rol existe
+        response = c.get('/roles/1/')
+        self.assertEquals(response.status_code, 200)
+        #eliminamos el rol
+        response = c.post('/roles/1/delete/', {'Confirmar':True}, follow=True)
+        self.assertRedirects(response, '/roles/')
+        #ahora ya no deberia existir el registro
+        response = c.get('/roles/1/')
+        self.assertEquals(response.status_code, 404)
