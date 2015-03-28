cfc3774f25715603a6a97898ff49f5f4c68e5594
jayalag <jayala1993@outlook.com>
2015-03-28 00:24:32 -0400

    Agregados tests de login

--- project/tests.py
+++ project/tests.py
@@ -1,4 +1,42 @@
 from django.test import TestCase
+from django.contrib.auth.models import User, Group, Permission
+from django.contrib.auth import SESSION_KEY
+
+class LoginTest(TestCase):
+    def setUp(self):
+        User.objects.create_user('test', 'test@test.com', 'test')
+
+    def test_login_existing_user(self):
+        u = User.objects.get(username='test')
+        self.assertIsNotNone(u)
+        c = self.client
+        login = c.login(username='test', password='test')
+        self.assertTrue(login)
+
+    def test_login_non_existing_user(self):
+        self.assertEqual(User.objects.filter(username='nobody').count(), 0)
+        c = self.client
+        login = c.login(username='nobody', password='nobody')
+        self.assertFalse(login)
+
+    def test_login_wrong_password(self):
+        u = User.objects.get(username='test')
+        self.assertIsNotNone(u)
+        c = self.client
+        pw = 'wrong'
+        self.assertFalse(u.check_password(pw))
+        login = c.login(username='test', password=pw)
+        self.assertFalse(login)
+
+    def test_logout_after_login(self):
+        u = User.objects.get(username='test')
+        self.assertIsNotNone(u)
+        c = self.client
+        login = c.login(username='test', password='test')
+        self.assertTrue(login)
+        c.logout()
+        self.assertTrue(SESSION_KEY not in self.client.session)
+
 
 class RolesTest(TestCase):
     def setUp(self):
