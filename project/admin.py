a4eed62518cbe31b80eb32e7dba20caf543df1a8
jayalag <jayala1993@outlook.com>
2015-04-07 19:26:12 -0400

    Sprint Agregado al Admin

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from project.models import Proyecto, MiembroEquipo, Flujo, Actividad, UserStory, Nota
+from project.models import Proyecto, MiembroEquipo, Flujo, Actividad, UserStory, Nota, Sprint
 
 admin.site.register(Proyecto)
 admin.site.register(MiembroEquipo)
@@ -7,3 +7,4 @@ admin.site.register(Flujo)
 admin.site.register(Actividad)
 admin.site.register(UserStory)
 admin.site.register(Nota)
+admin.site.register(Sprint)
