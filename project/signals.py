9b521675795ce418074653dc3e668e38d049472f
gperaltascura@gmail.com <gperaltascura@gmail.com>
2015-04-10 18:03:46 -0400

    agregada documentacion

--- project/signals.py
+++ project/signals.py
@@ -1,8 +1,14 @@
-
+# -*- coding: utf-8 -*-
 from guardian.shortcuts import assign_perm, get_perms, remove_perm
 
 
 def add_permissions_team_member(sender, **kwargs):
+    '''
+    Signal que se ejecuta cuando se agrega un rol a un miembro de equipo
+    que hace que los permisos asociados al rol se asigne.
+    :param sender: Clase que envia la signal
+    :param kwargs: Diccionario con parámetros
+    '''
     instance = kwargs['instance']
     action = kwargs['action']
     if(action=="post_add"):
