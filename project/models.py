4bc6fa1cf46e2b25730ef32072cd26e913cefb6a
jayalag <jayala1993@outlook.com>
2015-04-22 22:57:55 -0400

    Corregido error al guardar Sprint sin nombre

--- project/models.py
+++ project/models.py
@@ -102,18 +102,13 @@ class Sprint(models.Model):
     """
     Manejo de los sprints del proyecto
     """
-    nombre = models.CharField(max_length=20, blank=True, default='')
+    nombre = models.CharField(max_length=20)
     inicio = models.DateTimeField()
     fin = models.DateTimeField()
     proyecto = models.ForeignKey(Proyecto, null=False)
 
     class Meta:
         default_permissions = ()
-        
-    def save(self, *args, **kwargs):
-        if self.nombre == '':
-            self.nombre = "Sprint %d" % self.proyecto.sprint_set.count() + 1
-        super(Sprint, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.nombre
