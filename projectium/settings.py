28558bc5803ee317b2c70cddb58e30d8414e782b
jayalag <jayala1993@outlook.com>
2015-04-25 00:08:30 -0400

    Probando con sqlite

--- projectium/settings.py
+++ projectium/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'projectium.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'projectium',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
