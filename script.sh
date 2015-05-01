6d7bcbd3b4c65b6e32a95ec7ce5a0fdf584dc326
sanmiorto <sanmiorto@gmail.com>
2015-05-01 11:53:16 -0400

    pruebaDumdata: la creacion de dumdata por medio de python manage.py dumdata flujo_detail, flujo_list: agregado un boton para volver al proyecto userstory_detail.html: los nombres de user ahora estan pintados de acuerdo a su prioridad

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@
 sudo python CrearBasedeDatos.py
 sudo python manage.py makemigrations
 sudo python manage.py migrate
-sudo python manage.py loaddata fixtures/initial_data.json
+sudo python manage.py loaddata fixtures/pruebaDumpdata.json
