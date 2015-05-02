6a90c04de4929dd44d1c05340078c12b93229548
sanmiorto <sanmiorto@gmail.com>
2015-05-02 07:15:31 -0400

    script.sh: cambios

--- script.sh
+++ script.sh
@@ -1,6 +1,17 @@
-
+#! /bin/bash
 python CrearBasedeDatos.py
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata fixtures/initial_data.json
-python manage.py test
+if [ $? -eq 0 ];then
+    echo "Creando migraciones..."
+    python manage.py makemigrations
+    echo "Migrando base de datos..."
+    python manage.py migrate
+    echo "Poblando base de datos..."
+    python manage.py loaddata initial
+    #python manage.py loaddata fixtures/initial_data.json
+    echo "Creando versiones iniciales..."
+    python manage.py createinitialrevisions
+    echo "Ejecutando pruebas..."
+    #python -Wi manage.py test
+else
+    echo "Lo sentimos, no se pudo realizar la poblacion."
+fi manage.py test
