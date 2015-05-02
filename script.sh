3a199fca7100f4631a02876497ddab77ab219f54
sanmiorto <sanmiorto@gmail.com>
2015-05-02 06:36:01 -0400

    Cambios de Jordan

--- script.sh
+++ script.sh
@@ -7,6 +7,7 @@ if [ $? -eq 0 ];then
     python manage.py migrate
     echo "Poblando base de datos..."
     python manage.py loaddata initial
+
     #python manage.py loaddata fixtures/initial_data.json
     echo "Creando versiones iniciales..."
     python manage.py createinitialrevisions
