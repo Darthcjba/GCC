7b9504927e2cffedeffbfd493dbe1917fd555446
Jordan <jayala1993@outlook.com>
2015-05-10 16:40:08 -0400

    Refactorizacion de autenticacion

--- projectium/urls.py
+++ projectium/urls.py
@@ -2,12 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'projectium.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('authentication.urls', namespace='authentication')),
-    url(r'^', include('project.urls', namespace='project')),
-
-)
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^', include('authentication.urls', namespace='authentication')),
+                       url(r'^', include('project.urls', namespace='project')),
+                       )
