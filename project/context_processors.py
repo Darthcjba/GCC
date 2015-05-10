95c9b71480073ce0771da128dde4218bba3aaa15
Jordan <jayala1993@outlook.com>
2015-05-10 17:48:35 -0400

    Se agregan los user stories del usuario al contexto

--- project/context_processors.py
+++ project/context_processors.py
@@ -1,13 +1,18 @@
 from django.contrib.auth.decorators import login_required
-from project.models import Proyecto
+from project.models import Proyecto, UserStory
+
 
 def nav_context_processor(request):
     try:
         user = request.user
         if user.has_perm('project.list_all_projects'):
             nav_projects = Proyecto.objects.all()[:5]
+            nav_us = UserStory.objects.all()[:5]
         else:
             nav_projects = user.proyecto_set.all()[:5]
-        return {'nav_projects': nav_projects}
+            nav_us = user.userstory_set.all()[:5]
+        return {'nav_projects': nav_projects, 'nav_us': nav_us}
     except AttributeError:
-        return {'nav_projects': []}
+        return {'nav_projects': [],
+                'nav_us': [],
+                }
