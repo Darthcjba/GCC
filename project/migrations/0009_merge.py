221292b0688b8774d801652da7bd16fb08fe047e
Jordan <jayala1993@outlook.com>
2015-05-30 02:17:05 -0400

    Merge branch 'development' of https://github.com/UnPolloInc/Projectium into development
    # Conflicts:
    #	project/forms.py

---
+++ project/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('project', '0008_auto_20150528_2325'),
+        ('project', '0008_auto_20150528_2319'),
+    ]
+
+    operations = [
+    ]
