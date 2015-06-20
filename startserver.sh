6f8cf4b2962cea48fb811bdfa067da311981844c
sanmiorto <sanmiorto@gmail.com>
2015-06-20 09:04:44 -0400

    Movidos fixtures

--- startserver.sh
+++ startserver.sh
@@ -2,4 +2,4 @@
 sudo cp -r /home/santiortizpy/PycharmProjects/Projectium/ /var/www
 sudo service apache2 start>/dev/null
 echo Se ha iniciado el sistema.
-sudo phantomjs /var/www/Projectium/highchart-export/highcharts-convert.js -type png -host 127.0.0.1 -port 3003 >/dev/null  &
+sudo phantomjs /var/www/Projectium/highchart-export/highcharts-convert.js -type png -host 127.0.0.1 -port 3003
