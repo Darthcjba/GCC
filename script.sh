7b9301c0a35ff1c40d23cf56285d0f397a3e9da0
Jordan <jayala1993@outlook.com>
2015-06-20 01:49:03 -0400

    Add fixture con datos finales

--- script.sh
+++ script.sh
@@ -6,11 +6,12 @@ if [ $? -eq 0 ];then
     echo "Migrando base de datos..."
     python manage.py migrate
     echo "Poblando base de datos..."
-    python manage.py loaddata fechascambiadas
+    #python manage.py loaddata test2
+    python manage.py loaddata basededatosfinal
     #python manage.py loaddata init_with_files
     #python manage.py loaddata fixtures/initial_data.json
     echo "Creando versiones iniciales..."
-    python manage.py createinitialrevisions
+    #python manage.py createinitialrevisions
     echo "Ejecutando pruebas..."
     #python -Wi manage.py test
 else
